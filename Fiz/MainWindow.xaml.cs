1758152eb2d30217c2431041c21153cdc0f6a796
josephcassman <dev@null>
2022-05-08 11:54:09 -0700

    Code cleanup

--- Fiz/MainWindow.xaml.cs
+++ Fiz/MainWindow.xaml.cs
@@ -4,16 +4,9 @@ using System.Collections.ObjectModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Fiz {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window {
-        public MainWindow()
-        {
+        public MainWindow () {
             this.InitializeComponent();
         }
 
