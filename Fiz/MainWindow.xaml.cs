e81521ad125ea4ceed908d0d319a5197e057b283
josephcassman <dev@null>
2022-05-08 14:50:03 -0700

    Add clear all items to the command bar

--- Fiz/MainWindow.xaml.cs
+++ Fiz/MainWindow.xaml.cs
@@ -39,28 +39,34 @@ namespace Fiz {
 
         private async void AppBarButton_Click (object sender, RoutedEventArgs e) {
             var a = e.OriginalSource as AppBarButton;
-            if (a.Label == "Add") {
-                var window = new Microsoft.UI.Xaml.Window();
-                var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-                var picker = new Windows.Storage.Pickers.FileOpenPicker();
-                WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
-                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
-                picker.FileTypeFilter.Add(".jpg");
-                picker.FileTypeFilter.Add(".jpeg");
-                picker.FileTypeFilter.Add(".png");
-                picker.FileTypeFilter.Add(".gif");
-                picker.FileTypeFilter.Add(".mp4");
-                picker.FileTypeFilter.Add(".mp3");
-                var file = await picker.PickSingleFileAsync();
-                if (file != null) {
-                    MediaItems.Add(new MediaItem {
-                        File = file,
-                        FileName = file.Name,
-                    });
-                }
-                else {}
-                setVisibility();
+            switch (a.Label) {
+                case "Add":
+                    var window = new Microsoft.UI.Xaml.Window();
+                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                    WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+                    picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+                    picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
+                    picker.FileTypeFilter.Add(".jpg");
+                    picker.FileTypeFilter.Add(".jpeg");
+                    picker.FileTypeFilter.Add(".png");
+                    picker.FileTypeFilter.Add(".gif");
+                    picker.FileTypeFilter.Add(".mp4");
+                    picker.FileTypeFilter.Add(".mp3");
+                    var file = await picker.PickSingleFileAsync();
+                    if (file != null) {
+                        MediaItems.Add(new MediaItem {
+                            File = file,
+                            FileName = file.Name,
+                        });
+                    }
+                    else { }
+                    setVisibility();
+                    break;
+                case "Clear":
+                    MediaItems.Clear();
+                    setVisibility();
+                    break;
             }
         }
     }
