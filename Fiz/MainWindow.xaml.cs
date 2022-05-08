ca7b278335895b0f9a650adde55097aa89db47d2
josephcassman <dev@null>
2022-05-08 12:29:03 -0700

    Show a file picker dialog on the add button click event

--- Fiz/MainWindow.xaml.cs
+++ Fiz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.ObjectModel;
 using Windows.ApplicationModel.DataTransfer;
@@ -30,6 +31,32 @@ namespace Fiz {
 
         void setVisibility () {
             DropInstructions.Visibility = MediaItems.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private async void AppBarButton_Click (object sender, RoutedEventArgs e) {
+            var a = e.OriginalSource as AppBarButton;
+            if (a.Label == "Add") {
+                var window = new Microsoft.UI.Xaml.Window();
+                var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
+                picker.FileTypeFilter.Add(".jpg");
+                picker.FileTypeFilter.Add(".jpeg");
+                picker.FileTypeFilter.Add(".png");
+                picker.FileTypeFilter.Add(".gif");
+                picker.FileTypeFilter.Add(".mp4");
+                picker.FileTypeFilter.Add(".mp3");
+                var file = await picker.PickSingleFileAsync();
+                if (file != null) {
+                    MediaItems.Add(new MediaItem {
+                        File = file,
+                    });
+                }
+                else {}
+                setVisibility();
+            }
         }
     }
 
