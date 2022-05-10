1add3de23c37c81b13fcc30a38f1a7729e1d10f6
josephcassman <dev@null>
2022-05-10 15:16:38 -0700

    Add the tab bar

---
+++ src/ui/ViewModel/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UI.ViewModel {
+    public class MainViewModel : BindableBase {
+        private bool _pictureMode = true;
+        public bool PictureMode {
+            get => _pictureMode;
+            set => Set(ref _pictureMode, value);
+        }
+    }
+}
