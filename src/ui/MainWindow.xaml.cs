a357816d3ef95092668dd1c4a6bf329ecd32707f
josephcassman <dev@null>
2022-05-10 15:16:38 -0700

    Add the tab bar

--- src/ui/MainWindow.xaml.cs
+++ src/ui/MainWindow.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using UI.ViewModel;
 
-namespace src
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace UI {
+    public partial class MainWindow : Window {
+        public MainWindow () {
             InitializeComponent();
+            DataContext = vm;
+        }
+
+        public MainViewModel vm => App.ViewModel;
+
+        private void Picture_Click (object sender, RoutedEventArgs e) {
+            vm.PictureMode = false;
+        }
+
+        private void Video_Click (object sender, RoutedEventArgs e) {
+            vm.PictureMode = true;
         }
     }
 }
---
+++ src/ui/ViewModel/Converters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UI.ViewModel {
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InvertConverter : IValueConverter {
+        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool a = (bool) value;
+            return !a;
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            var a = (bool) value;
+            return !a;
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class VisibleWhenFalseConverter : IValueConverter {
+        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool a = (bool) value;
+            return a ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            var a = (Visibility) value;
+            return a == Visibility.Collapsed;
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class VisibleWhenTrueConverter : IValueConverter {
+        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool a = (bool) value;
+            return a ? Visibility.Visible: Visibility.Collapsed;
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            var a = (Visibility) value;
+            return a == Visibility.Visible;
+        }
+    }
+}
