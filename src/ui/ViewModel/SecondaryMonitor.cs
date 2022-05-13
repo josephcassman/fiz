5eb4204654f851b30c806a45afe1acd212b4c4d4
josephcassman <dev@null>
2022-05-12 21:58:27 -0700

    Generalize second monitor logic to work with multiple window types

--- src/ui/ViewModel/SecondaryMonitor.cs
+++ src/ui/ViewModel/SecondaryMonitor.cs
@@ -3,30 +3,37 @@ using System.Windows;
 
 namespace UI.ViewModel {
     public static class SecondMonitor {
-        public static void ShowMaximizedOnSecondScreen (Window window, MainViewModel vm) {
-            var a = System.Windows.Forms.Screen.AllScreens.Where(a => !a.Primary);
-            if (a.Any()) {
-                if (!window.IsLoaded)
-                    window.WindowStartupLocation = WindowStartupLocation.Manual;
-
-                var b = a.First();
-
-                window.Left = b.WorkingArea.Left;
-                window.Top = b.WorkingArea.Top;
-                window.Width = b.WorkingArea.Width;
-                window.Height = b.WorkingArea.Height;
-
-                window.Show();
-
-                if (window.IsLoaded)
-                    window.WindowState = WindowState.Maximized;
-            }
-            else {
-                vm.ShowMediaOnSecondMonitor = false;
+        public static void ShowMediaWindow (Window window, MainViewModel vm) {
+            void showOnPrimaryMonitor () {
                 window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
                 window.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height / 2;
                 window.Width = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width / 2;
                 window.Show();
+            }
+
+            var a = System.Windows.Forms.Screen.AllScreens.Where(a => !a.Primary);
+            if (a.Any()) {
+                if (!vm.ShowMediaOnSecondMonitor) showOnPrimaryMonitor();
+                else {
+                    if (!window.IsLoaded)
+                        window.WindowStartupLocation = WindowStartupLocation.Manual;
+
+                    var b = a.First();
+
+                    window.Left = b.WorkingArea.Left;
+                    window.Top = b.WorkingArea.Top;
+                    window.Width = b.WorkingArea.Width;
+                    window.Height = b.WorkingArea.Height;
+
+                    window.Show();
+
+                    if (window.IsLoaded)
+                        window.WindowState = WindowState.Maximized;
+                }
+            }
+            else {
+                vm.ShowMediaOnSecondMonitor = false;
+                showOnPrimaryMonitor();
             }
         }
     }
