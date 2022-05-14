896e5c984fef74e44718433f5bfc1141efd43bc5
josephcassman <dev@null>
2022-05-14 13:55:59 -0700

    Remove unused code

--- src/ui/ViewModel/Converters.cs
+++ src/ui/ViewModel/Converters.cs
@@ -3,19 +3,6 @@ using System.Windows;
 using System.Windows.Data;
 
 namespace UI.ViewModel {
-    [ValueConversion(typeof(bool), typeof(bool))]
-    public class InvertConverter : IValueConverter {
-        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            bool a = (bool) value;
-            return !a;
-        }
-
-        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            var a = (bool) value;
-            return !a;
-        }
-    }
-
     [ValueConversion(typeof(bool), typeof(Visibility))]
     public class VisibleWhenFalseConverter : IValueConverter {
         public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
