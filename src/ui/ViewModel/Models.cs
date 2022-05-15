0e4b3ce73cefcf24f8c7ccb2c0f04c1d98a7b0ff
josephcassman <dev@null>
2022-05-15 13:21:03 -0700

    Add videos to the media list

--- src/ui/ViewModel/Models.cs
+++ src/ui/ViewModel/Models.cs
@@ -1,10 +1,19 @@
-﻿using System.Windows.Media.Imaging;
+﻿using System;
+using System.Windows.Media.Imaging;
 
 namespace UI.ViewModel {
-    public class PictureItem {
+    public abstract class MediaItem {
         public string Name { get; set; } = "";
         public string Path { get; set; } = "";
         public BitmapImage Preview { get; set; } = new();
+        public bool IsPicture { get; set; } = true;
+    }
+
+    public class PictureItem : MediaItem {
         public BitmapImage Media { get; set; } = new();
+    }
+
+    public class VideoItem : MediaItem {
+        public Uri Media { get; set; } = new("about:blank");
     }
 }
