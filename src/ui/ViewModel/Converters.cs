ebca96d730a8effa51f15a83a7a956a4900610dc
josephcassman <dev@null>
2022-05-17 13:17:58 -0700

    Code cleanup

--- src/ui/ViewModel/Converters.cs
+++ src/ui/ViewModel/Converters.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
 namespace UI.ViewModel {
     [ValueConversion(typeof(bool), typeof(Visibility))]
     public class VisibleWhenFalseConverter : IValueConverter {
-        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
             bool a = (bool) value;
             return a ? Visibility.Collapsed : Visibility.Visible;
         }
 
-        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
             var a = (Visibility) value;
             return a == Visibility.Collapsed;
         }
@@ -18,12 +19,12 @@ namespace UI.ViewModel {
 
     [ValueConversion(typeof(bool), typeof(Visibility))]
     public class VisibleWhenTrueConverter : IValueConverter {
-        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
             bool a = (bool) value;
             return a ? Visibility.Visible: Visibility.Collapsed;
         }
 
-        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
             var a = (Visibility) value;
             return a == Visibility.Visible;
         }
@@ -31,24 +32,24 @@ namespace UI.ViewModel {
 
     [ValueConversion(typeof(MediaItem), typeof(Visibility))]
     public class MediaItemToVisibleWhenTrueConverter : IValueConverter {
-        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
             var a = (MediaItem) value;
             return string.IsNullOrEmpty(a.Name) ? Visibility.Hidden : Visibility.Visible;
         }
 
-        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
             throw new NotImplementedException();
         }
     }
 
     [ValueConversion(typeof(MediaItem), typeof(Visibility))]
     public class MediaItemToHiddenWhenTrueConverter : IValueConverter {
-        public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
             var a = (MediaItem) value;
             return string.IsNullOrEmpty(a.Name) ? Visibility.Visible : Visibility.Hidden;
         }
 
-        public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture) {
             throw new NotImplementedException();
         }
     }
