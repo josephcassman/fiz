159764b77f5be6e0ac15209c0da2a3a527e1de50
josephcassman <dev@null>
2022-05-18 10:28:59 -0700

    Code cleanup

--- src/ui/ViewModel/MainViewModel.cs
+++ src/ui/ViewModel/MainViewModel.cs
@@ -6,15 +6,23 @@ namespace UI.ViewModel {
     public class MainViewModel : BindableBase {
         public MainViewModel() {
             SettingsStorage.Initialize();
-            ShowMediaOnSecondMonitor = SettingsStorage.ShowMediaOnSecondMonitor;
             ShowMediaFullscreen = SettingsStorage.ShowMediaFullscreen;
+            ShowMediaOnSecondMonitor = SettingsStorage.ShowMediaOnSecondMonitor;
         }
 
-        public ObservableCollection<MediaItem> MediaItems = new();
+        public void MoveToPreviousMediaItem () { MoveUp?.Invoke(this, new()); }
+        public void MoveToNextMediaItem () { MoveDown?.Invoke(this, new()); }
 
         public event EventHandler? StopVideo;
         public event EventHandler? MoveUp;
         public event EventHandler? MoveDown;
+
+        public void AddMediaItem (MediaItem a) {
+            MediaItems.Add(a);
+            MediaListHasContents = true;
+        }
+
+        public ObservableCollection<MediaItem> MediaItems = new();
 
         int _mediaItemsCurrentIndex = -1;
         public int MediaItemsCurrentIndex {
@@ -34,43 +42,7 @@ namespace UI.ViewModel {
             }
         }
 
-        bool _pictureDisplayedOnMediaWindow = true;
-        public bool PictureDisplayedOnMediaWindow {
-            get => _pictureDisplayedOnMediaWindow;
-            set => Set(ref _pictureDisplayedOnMediaWindow, value);
-        }
-
-        BitmapImage _currentPicture = new();
-        public BitmapImage CurrentPicture {
-            get => _currentPicture;
-            set => Set(ref _currentPicture, value);
-        }
-
-        Uri _currentVideo = new("about:blank");
-        public Uri CurrentVideo {
-            get => _currentVideo;
-            set => Set(ref _currentVideo, value);
-        }
-
-        VideoItem _singleVideo = new();
-        public VideoItem SingleVideo {
-            get => _singleVideo;
-            set => Set(ref _singleVideo, value);
-        }
-
-        public void MoveToPreviousMediaItem () { MoveUp?.Invoke(this, new()); }
-        public void MoveToNextMediaItem () { MoveDown?.Invoke(this, new()); }
-
-        public void AddMediaItem (MediaItem a) {
-            MediaItems.Add(a);
-            MediaListHasContents = true;
-        }
-
-        bool _videoPaused = true;
-        public bool VideoPaused {
-            get => _videoPaused;
-            set => Set(ref _videoPaused, value);
-        }
+        // Main window state
 
         bool _mediaListHasContents = false;
         public bool MediaListHasContents {
@@ -84,10 +56,53 @@ namespace UI.ViewModel {
             set => Set(ref _mediaListMode, value);
         }
 
+        // Media window state
+
+        BitmapImage _currentPicture = new();
+        public BitmapImage CurrentPicture {
+            get => _currentPicture;
+            set => Set(ref _currentPicture, value);
+        }
+
+        Uri _currentVideo = new("about:blank");
+        public Uri CurrentVideo {
+            get => _currentVideo;
+            set => Set(ref _currentVideo, value);
+        }
+
         bool _mediaDisplayed = false;
         public bool MediaDisplayed {
             get => _mediaDisplayed;
             set => Set(ref _mediaDisplayed, value);
+        }
+
+        bool _pictureDisplayedOnMediaWindow = true;
+        public bool PictureDisplayedOnMediaWindow {
+            get => _pictureDisplayedOnMediaWindow;
+            set => Set(ref _pictureDisplayedOnMediaWindow, value);
+        }
+
+        VideoItem _singleVideo = new();
+        public VideoItem SingleVideo {
+            get => _singleVideo;
+            set => Set(ref _singleVideo, value);
+        }
+
+        bool _videoPaused = true;
+        public bool VideoPaused {
+            get => _videoPaused;
+            set => Set(ref _videoPaused, value);
+        }
+
+        // Settings
+
+        bool _showMediaFullscreen = true;
+        public bool ShowMediaFullscreen {
+            get => _showMediaFullscreen;
+            set {
+                Set(ref _showMediaFullscreen, value);
+                SettingsStorage.ShowMediaFullscreen = value;
+            }
         }
 
         bool _showMediaOnSecondMonitor = true;
@@ -96,15 +111,6 @@ namespace UI.ViewModel {
             set {
                 Set(ref _showMediaOnSecondMonitor, value);
                 SettingsStorage.ShowMediaOnSecondMonitor = value;
-            }
-        }
-
-        bool _showMediaFullscreen = true;
-        public bool ShowMediaFullscreen {
-            get => _showMediaFullscreen;
-            set {
-                Set(ref _showMediaFullscreen, value);
-                SettingsStorage.ShowMediaFullscreen = value;
             }
         }
     }
