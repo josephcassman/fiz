24b2e43057c2fc420c803c7303b7cc51395f92f7
josephcassman <dev@null>
2022-06-05 12:23:35 -0700

    Update the single video preview UI

--- src/ui/ViewModel/Models.cs
+++ src/ui/ViewModel/Models.cs
@@ -5,7 +5,6 @@ namespace UI.ViewModel {
     public abstract class MediaItem {
         public string Name { get; set; } = "";
         public string Path { get; set; } = "";
-        public BitmapSource Preview { get; set; } = new BitmapImage();
         public abstract bool IsPdf { get; }
         public abstract bool IsPicture { get; }
         public abstract bool IsVideo { get; }
@@ -19,6 +18,7 @@ namespace UI.ViewModel {
     }
 
     public class PictureItem : MediaItem {
+        public BitmapSource Preview { get; set; } = new BitmapImage();
         public BitmapImage Media { get; set; } = new();
         public override bool IsPdf => false;
         public override bool IsPicture => true;
@@ -28,6 +28,7 @@ namespace UI.ViewModel {
     public class VideoItem : MediaItem {
         public Uri Media { get; set; } = new("about:blank");
         public TimeSpan Skip { get; set; } = TimeSpan.Zero;
+        public TimeSpan TotalLength { get; set; } = TimeSpan.Zero;
         public override bool IsPdf => false;
         public override bool IsPicture => false;
         public override bool IsVideo => true;
