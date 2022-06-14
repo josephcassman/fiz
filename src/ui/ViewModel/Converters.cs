75ae194a77bb26176d5d151106112f0e7ba7226c
josephcassman <dev@null>
2022-06-14 12:43:09 -0700

    Use one WebView2 control for web and document content

--- src/ui/ViewModel/Converters.cs
+++ src/ui/ViewModel/Converters.cs
@@ -41,6 +41,18 @@ namespace UI.ViewModel {
         }
     }
 
+    [ValueConversion(typeof(bool), typeof(double))]
+    public sealed class DisjunctionVisibleConverter : IMultiValueConverter {
+        public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture) {
+            return values.Where(value => value is bool).Any(value => (bool) value == true) ?
+                  Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack (object value, Type[] targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
     [ValueConversion(typeof(bool), typeof(bool))]
     public sealed class InvertConverter : IValueConverter {
         public object Convert (object value, Type targetType, object parameter, CultureInfo culture) {
