09a4135d0c59781ddf4b8ac06e72cbb09eee3c78
josephcassman <dev@null>
2023-01-17 19:18:05 -0800

    Remove PDF features
    Simplifying the app to focus on displaying image, video, and
    web page media.

--- src/ui/ViewModel/Models.cs
+++ src/ui/ViewModel/Models.cs
@@ -5,28 +5,18 @@ namespace UI.ViewModel {
     public abstract class MediaItem {
         public string FileName { get; set; } = "";
         public string FilePath { get; set; } = "";
-        public abstract bool IsPdf { get; }
         public abstract bool IsPicture { get; }
         public abstract bool IsVideo { get; }
     }
 
-    public sealed class PdfItem : MediaItem {
-        public Uri Source { get; set; } = new("about:blank");
-        public override bool IsPdf => true;
-        public override bool IsPicture => false;
-        public override bool IsVideo => false;
-    }
-
     public sealed class PictureItem : MediaItem {
         public BitmapImage Source { get; set; } = new();
-        public override bool IsPdf => false;
         public override bool IsPicture => true;
         public override bool IsVideo => false;
     }
 
     public sealed class VideoItem : MediaItem {
         public Uri Source { get; set; } = new("about:blank");
-        public override bool IsPdf => false;
         public override bool IsPicture => false;
         public override bool IsVideo => true;
     }
